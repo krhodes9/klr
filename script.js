7a6d0a958d56949d549f31967fd49ac537bb3e06
krhodes9 <61336730+krhodes9@users.noreply.github.com>
2021-05-11 21:48:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-require(["esri/Map", "esri/views/MapView"], function(Map, MapView) {
-        var map = new Map({
-          basemap: "dark-gray"
+require([
+        "esri/Map",
+        "esri/layers/FeatureLayer",
+        "esri/layers/GeoJSONLayer",
+        "esri/views/MapView",
+         "esri/widgets/Legend",
+        "esri/widgets/Expand",
+        "esri/widgets/Home",
+        "esri/WebMap",
+      ], function(Map, FeatureLayer, GeoJSONLayer, MapView, Legend, Expand, Home, WebMap) {
+
+          var featureLayer = new FeatureLayer({
+          url: "https://services2.arcgis.com/bB9Y1bGKerz1PTl5/arcgis/rest/services/Madison_WI_City_Wards/FeatureServer"
         });
+        
 
         var view = new MapView({
           container: "viewDiv",
           map: map,
           zoom: 10,
-          center: [-90, 38] // longitude, latitude
+          center: [-89, 48] // longitude, latitude
         });
       });
